962a8de163a1800ccbee0f575fc35490cbd93035
mbachett <matteo.bachetti@irap.omp.eu>
2014-11-02 10:27:44 +0100

    New program to save (c)pdss in a format readable to XSpec

---
+++ mp_save_as_xspec.py
@@ -0,0 +1,35 @@
+from __future__ import division, print_function
+from mp_base import mp_get_file_type
+import numpy as np
+
+
+def mp_save_as_xspec(fname):
+    ftype, contents = mp_get_file_type(fname)
+    if 'freq' in contents.keys():
+        freq = contents['freq']
+        pds = contents[ftype]
+        epds = contents['e' + ftype]
+        outname = fname.replace('.p', '_xsp.qdp')
+        df = freq[1] - freq[0]
+        np.savetxt(outname, np.transpose([freq.real - df / 2,
+                                          freq.real + df / 2,
+                                          pds.real * df, epds.real * df]))
+    elif 'flo' in contents.keys():
+        ftype = ftype.replace('reb', '')
+        flo = contents['flo']
+        fhi = contents['fhi']
+        pds = contents[ftype]
+        epds = contents['e' + ftype]
+        outname = fname.replace('.p', '_xsp.qdp')
+        df = fhi - flo
+        np.savetxt(outname, np.transpose([flo.real, fhi.real,
+                                          pds.real * df, epds.real * df]))
+    else:
+        raise Exception('File type not recognized')
+
+
+if __name__ == '__main__':
+    import sys
+    fnames = sys.argv[1:]
+    for f in fnames:
+        mp_save_as_xspec(f)
