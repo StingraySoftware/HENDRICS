7922e55a6809d36b91669b6c0dae2dde9dfc2f50
mbachett <matteo.bachetti@irap.omp.eu>
2014-11-02 10:27:10 +0100

    New program and library to rebin lcs and pdss

---
+++ mp_rebin.py
@@ -0,0 +1,152 @@
+from __future__ import division, print_function
+import numpy as np
+from mp_base import mp_get_file_type
+
+
+def mp_const_rebin(x, y, factor, yerr=None, normalize=True):
+    '''Rebins any pair of variables. Might be time and counts, or freq and pds.
+    Also possible to rebin the error on y.
+        '''
+    if factor <= 1:
+        res = [x, y]
+        if yerr is not None:
+            res.append(yerr)
+        return res
+
+    nbin = len(y)
+
+    arr_dtype = y.dtype
+    new_nbins = np.int(nbin / factor)
+
+    y_resh = np.reshape(y[:new_nbins * factor], (new_nbins, factor))
+    x_resh = np.reshape(x[:new_nbins * factor], (new_nbins, factor))
+
+    new_y = np.sum(y_resh, axis=1)
+    new_x = np.sum(x_resh, axis=1) / factor
+
+    if yerr is not None:
+        yerr_resh = np.reshape(yerr[:new_nbins * factor], (new_nbins, factor))
+        new_yerr = np.sum(yerr_resh, axis=1)
+    else:
+        new_yerr = np.zeros(len(new_x), dtype=arr_dtype)
+
+    if normalize:
+        return new_x, new_y / factor, new_yerr / factor
+    else:
+        return new_x, new_y, new_yerr
+
+
+def mp_geom_bin(freq, pds, pds_err=None, npds=None,
+                bin_factor=None, verbose=0, return_nbins=False):
+    '''
+    Given a PDS, bin it geometrically. Freely taken from the algorithm
+    in isisscripts.sl
+    '''
+    from numpy import log10
+
+    df = freq[1] - freq[0]
+    fmin = min(freq)
+    fmax = max(freq)
+    if npds is None:
+        npds = 1.
+    if pds_err is None:
+        pds_err = np.zeros(len(pds))
+    if bin_factor <= 1:
+        "Bin factor must be > 1!!"
+        f0 = freq - df / 2.
+        f1 = freq + df / 2.
+        retval = [f0, f1, pds, pds_err]
+        if return_nbins:
+            retval.append(np.ones(len(pds)) * npds)
+        return retval
+
+    logstep = log10(bin_factor)
+#    maximum number of bins
+    nmax = np.int((log10(fmax) - log10(fmin)) / logstep + 0.5)
+
+# Low frequency grid
+    flo = fmin * 10 ** (np.array(range(nmax)) * logstep)
+    flo = np.append(flo, [fmax])
+
+# Now the clever part: building a histogram of frequencies
+    pds_dtype = pds.dtype
+    pdse_dtype = pds_err.dtype
+    bins = np.digitize(freq, flo)
+    newpds = np.zeros(nmax, dtype=pds_dtype) - 1
+    newpds_err = np.zeros(nmax, dtype=pdse_dtype)
+    newfreqlo = np.zeros(nmax)
+    new_nbins = np.zeros(nmax, dtype=long)
+    for i in range(nmax):
+        good = bins == i
+        ngood = np.count_nonzero(good)
+        new_nbins[i] = ngood
+        if ngood == 0:
+            continue
+        newpds[i] = np.sum(pds[good]) / ngood
+        newfreqlo[i] = np.min(freq[good])
+        newpds_err[i] = np.sqrt(np.sum(pds_err[good] ** 2)) / ngood
+    good = new_nbins > 0
+    new_nbins = new_nbins[good] * npds
+    newfreqlo = newfreqlo[good]
+    newpds = newpds[good]
+    newpds_err = newpds_err[good]
+    newfreqhi = newfreqlo[1:]
+    newfreqhi = np.append(newfreqhi, [fmax])
+
+    retval = [newfreqlo, newfreqhi, newpds, newpds_err]
+    if return_nbins:
+        retval.append(new_nbins)
+    return retval
+
+
+def mp_rebin_file(filename, rebin):
+    ftype, contents = mp_get_file_type(filename)
+    if ftype == 'lc':
+        x = contents['time']
+        y = contents['lc']
+        ye = np.sqrt(y)
+        x, y, ye = \
+            mp_const_rebin(x, y, rebin, ye, normalize=False)
+        contents['time'] = x
+        contents['lc'] = y
+
+    if ftype in ['pds', 'cpds']:
+        x = contents['freq']
+        y = contents[ftype]
+        ye = contents['e' + ftype]
+        # if rebin is integer, use constant rebinning. Otherwise, geometrical
+        if rebin == float(int(rebin)):
+            x, y, ye = \
+                mp_const_rebin(x, y, rebin, ye, normalize=True)
+            contents['freq'] = x
+            contents[ftype] = y
+            contents['e' + ftype] = ye
+        else:
+            x1, x2, y, ye, nbin = \
+                mp_geom_bin(x, y, rebin, ye, return_nbins=True)
+            del contents['freq']
+            contents['flo'] = x1
+            contents['fhi'] = x2
+            contents[ftype] = y
+            contents['e' + ftype] = ye
+            contents['nbins'] = nbin
+
+    outfile = f.replace('.p', '_rebin%g.p' % rebin)
+    print('Saving light curve to %s' % outfile)
+    pickle.dump(contents, open(outfile, 'wb'))
+
+
+if __name__ == '__main__':
+    import argparse
+    import cPickle as pickle
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("files", help="List of light curve files", nargs='+')
+    parser.add_argument("-r", "--rebin", type=float, default=1,
+                        help="(C)PDS rebinning to apply")
+
+    args = parser.parse_args()
+
+    rebin = args.rebin
+    for f in args.files:
+        mp_rebin_file(f, rebin)
