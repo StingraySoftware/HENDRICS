d32276f9afe74787ea5b2712edb3ccea7c075924
mbachett <matteo.bachetti@irap.omp.eu>
2014-11-03 01:22:44 +0100

    Minor changes

--- mp_save_as_xspec.py
+++ mp_save_as_xspec.py
@@ -5,25 +5,29 @@ import numpy as np
 
 def mp_save_as_xspec(fname):
     ftype, contents = mp_get_file_type(fname)
+
+    outname = fname.replace('.p', '_xsp.qdp')
+
     if 'freq' in contents.keys():
         freq = contents['freq']
         pds = contents[ftype]
         epds = contents['e' + ftype]
-        outname = fname.replace('.p', '_xsp.qdp')
         df = freq[1] - freq[0]
+
         np.savetxt(outname, np.transpose([freq.real - df / 2,
                                           freq.real + df / 2,
-                                          pds.real * df, epds.real * df]))
+                                          pds.real * df,
+                                          epds.real * df]))
     elif 'flo' in contents.keys():
         ftype = ftype.replace('reb', '')
         flo = contents['flo']
         fhi = contents['fhi']
         pds = contents[ftype]
         epds = contents['e' + ftype]
-        outname = fname.replace('.p', '_xsp.qdp')
         df = fhi - flo
         np.savetxt(outname, np.transpose([flo.real, fhi.real,
-                                          pds.real * df, epds.real * df]))
+                                          pds.real * df,
+                                          epds.real * df]))
     else:
         raise Exception('File type not recognized')
 
