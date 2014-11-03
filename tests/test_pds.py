0fc331940dfc6dfa16b74b25a09ded6520274da0
mbachett <matteo.bachetti@irap.omp.eu>
2014-11-03 13:13:27 +0100

    Just another kind of test

--- tests/test_pds.py
+++ tests/test_pds.py
@@ -9,7 +9,9 @@ pdsdata = pickle.load(open(sys.argv[1]))
 freq = pdsdata['freq']
 pds = pdsdata['pds']
 
-plt.loglog(freq[1:], freq[1:] * (pds[1:] - np.mean(pds[len(pds) / 2:])),
-           drawstyle='steps-mid')
+#plt.loglog(freq[1:], freq[1:] * (pds[1:] - np.mean(pds[len(pds) / 2:])),
+#           drawstyle='steps-mid')
+plt.plot(freq[1:], pds[1:],
+         drawstyle='steps-mid')
 
 plt.show()
