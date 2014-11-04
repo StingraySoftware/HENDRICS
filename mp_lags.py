9b3b5ebbdd52f92e1cfbf4fb48b64659f13f7947
mbachett <matteo.bachetti@irap.omp.eu>
2014-11-04 15:31:15 +0100

    Implements lag calculations. Resolves Issue #10

--- mp_lags.py
+++ mp_lags.py
@@ -1,10 +1,19 @@
 from __future__ import division, print_function
-#from mp_fspec import mp_calc_lags
+from mp_fspec import mp_calc_lags, mp_read_fspec
 
 
 def mp_lags_from_spectra(cpdsfile, pds1file, pds2file):
-    print ('mp_lags_from_spectra not yet implemented')
-    return
+    print ('mp_lags_from_spectra Needs testing')
+
+    ftype, cfreq, cpds, ecpds, nchunks, rebin = mp_read_fspec(cpdsfile)
+    ftype, p1freq, pds1, epds1, nchunks, rebin = mp_read_fspec(pds1file)
+    ftype, p2freq, pds2, epds2, nchunks, rebin = mp_read_fspec(pds2file)
+
+    assert len(cpds) == len(pds1), 'Files are not compatible'
+    assert len(cpds) == len(pds2), 'Files are not compatible'
+
+    lags, elags = mp_calc_lags(cfreq, cpds, pds1, pds2, nchunks, rebin)
+    return lags, elags
 
 
 if __name__ == '__main__':
