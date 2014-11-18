a5ded2d1fa2a7aea9d5002de387632694c0f737d
mbachett <matteo.bachetti@irap.omp.eu>
2014-11-18 22:12:25 +0100

    Now also plots a detection level that depends on the binning of data

--- tests/test_pds.py
+++ tests/test_pds.py
@@ -3,6 +3,8 @@ import cPickle as pickle
 import sys
 import matplotlib.pyplot as plt
 from maltpynt.mp_base import mp_detection_level
+import collections
+import numpy as np
 
 
 if __name__ == '__main__':
@@ -19,16 +21,27 @@ if __name__ == '__main__':
     epds = pdsdata['epds']
     npds = pdsdata['npds']
     norm = pdsdata['norm']
+    rebin = pdsdata['rebin']
 
     nbin = len(pds[1:])
+
+    meanp = np.mean(pds[len(pds)//2:])
+    lev = mp_detection_level(nbin, n_summed_spectra=npds, n_rebin=rebin)
+
+    if meanp < 2:
+        print ('Renormalizing PDS because of PDS average < 2')
+        print ('The procedure is not failproof. Beware of wrong results')
+#        lev = lev / 2 * meanp
+        pds = pds / meanp * 2
+    if isinstance(lev, collections.Iterable):
+        plt.plot(freq, lev)
+    else:
+        plt.axhline(lev)
 
     plt.plot(freq[1:], pds[1:],
              drawstyle='steps-mid')
     plt.errorbar(freq[1:], pds[1:], yerr=epds[1:],
                  drawstyle='steps-mid', fmt='-')
-
-    lev = mp_detection_level(nbin, n_summed_spectra=npds)
-    plt.axhline(lev)
 
     plt.xlabel('Frequency')
     if norm == 'rms':
