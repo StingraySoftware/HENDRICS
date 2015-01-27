f39fd32a122df46ea17c625e2a541b52dfc2002e
mbachett <matteo.bachetti@irap.omp.eu>
2015-01-27 12:35:39 +0100

    Calculates phase of a pulsed signal Remove unused library file

--- maltpynt/__init__.py
+++ maltpynt/__init__.py
@@ -7,4 +7,3 @@ from . import mp_read_events as read_events
 from . import mp_rebin as rebin
 from . import mp_save_as_xspec as save_as_xspec
 from . import mp_io as io
-from . import mp_fold as fold
