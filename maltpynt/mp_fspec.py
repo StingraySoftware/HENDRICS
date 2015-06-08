Corrects bug in dynamical cross power spectra (incorrect shape of saved data, incorrect number of bins. Thanks Felix for the report\!)

maltpynt/mp_fspec.py
@@ -502,7 +502,7 @@ def mp_calc_cpds(lcfile1, lcfile2, fftlen,
 
     logging.debug(repr(results.dyncpds))
     if save_dyn:
-        outdata["dyncpds"] = np.array(results.dyncpds)
+        outdata["dyncpds"] = np.array(results.dyncpds)[:, 1:]
         outdata["dyntimes"] = np.array(results.times)
 
     logging.info('Saving CPDS to %s' % outname)

maltpynt/mp_io.py
@@ -109,7 +109,7 @@ def mp_read_from_netcdf(fname):
         values = dum.__array__()
         # Handle special case of complex
         if dum.dtype == cpl128:
-            arr = np.empty(values.size, dtype=np.complex128)
+            arr = np.empty(values.shape, dtype=np.complex128)
             arr.real = values[str('real')]
             arr.imag = values[str('imag')]
             values = arr