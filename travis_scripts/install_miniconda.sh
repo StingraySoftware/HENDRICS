fa7eb7c2e2bd330d6aac75b64d8d9bbc2e3c30d8
mbachett <matteo.bachetti@irap.omp.eu>
2015-07-14 18:00:50 +0200

    Test if cache exists

--- travis_scripts/install_miniconda.sh
+++ travis_scripts/install_miniconda.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b
+if test ! -e /home/travis/miniconda; then
+    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+    chmod +x miniconda.sh
+    ./miniconda.sh -b
+fi
