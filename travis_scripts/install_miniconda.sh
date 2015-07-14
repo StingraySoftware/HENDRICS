61838addc5c037f26afc462659174bbc948f3639
mbachett <matteo.bachetti@irap.omp.eu>
2015-07-14 18:30:51 +0200

    Let's see how travis behaves now

--- travis_scripts/install_miniconda.sh
+++ travis_scripts/install_miniconda.sh
@@ -5,7 +5,7 @@ WITH_NETCDF=$2
 if test ! -e /home/travis/miniconda/pkgs/; then
     wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
     chmod +x miniconda.sh
-    ./miniconda.sh -b
+    ./miniconda.sh -b -f
     export PATH=/home/travis/miniconda/bin:$PATH
     conda update --yes conda
     if test WITH_NETCDF = "yes"; then
