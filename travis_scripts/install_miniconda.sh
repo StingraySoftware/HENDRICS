fec7ed235aef4d4d5e53a3e2027c82e58cba709d
mbachett <matteo.bachetti@irap.omp.eu>
2015-07-14 19:15:06 +0200

    Corrects another bug in condition

--- travis_scripts/install_miniconda.sh
+++ travis_scripts/install_miniconda.sh
@@ -6,14 +6,13 @@ if test ! -e /home/travis/miniconda/pkgs/; then
     wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
     chmod +x miniconda.sh
     ./miniconda.sh -b -f
-    export PATH=/home/travis/miniconda/bin:$PATH
-    conda update --yes conda
-    if test $WITH_NETCDF = "yes"; then
-        conda install --yes python=$TRAVIS_PYTHON_VERSION libnetcdf hdf5 h5py netCDF4 scipy matplotlib astropy numpy
-    else
-        conda install --yes python=$TRAVIS_PYTHON_VERSION scipy matplotlib astropy numpy
-    fi
-else:
-    export PATH=/home/travis/miniconda/bin:$PATH
-    conda update --yes conda    
+fi
+
+export PATH=/home/travis/miniconda/bin:$PATH
+conda update --yes conda
+
+if test $WITH_NETCDF = "yes"; then
+    conda install --yes python=$TRAVIS_PYTHON_VERSION libnetcdf hdf5 h5py netCDF4 scipy matplotlib astropy numpy
+else
+    conda install --yes python=$TRAVIS_PYTHON_VERSION scipy matplotlib astropy numpy
 fi
