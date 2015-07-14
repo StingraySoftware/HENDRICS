fc1732ef4375ec69f65d31a6b9ad263aa98ccae7
mbachett <matteo.bachetti@irap.omp.eu>
2015-07-14 18:20:12 +0200

    Let's see how travis behaves now

--- travis_scripts/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TRAVIS_PYTHON_VERSION=$1
-conda install --yes python=$TRAVIS_PYTHON_VERSION scipy matplotlib astropy
--- travis_scripts/install_netcdf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TRAVIS_PYTHON_VERSION=$1
-conda install --yes python=$TRAVIS_PYTHON_VERSION libnetcdf hdf5 h5py netCDF4 scipy matplotlib astropy
