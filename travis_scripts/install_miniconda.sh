d218238deda26751992af70a6a7f40828df7a3bb
mbachett <matteo.bachetti@irap.omp.eu>
2015-07-14 18:19:18 +0200

    Probable better script

--- travis_scripts/install_miniconda.sh
+++ travis_scripts/install_miniconda.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+TRAVIS_PYTHON_VERSION=$1
+WITH_NETCDF=$2
+
 if test ! -e /home/travis/miniconda; then
     wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
     chmod +x miniconda.sh
     ./miniconda.sh -b
+    export PATH=/home/travis/miniconda/bin:$PATH
+    conda update --yes conda
+    if test WITH_NETCDF = "yes"; then
+        conda install --yes python=$TRAVIS_PYTHON_VERSION libnetcdf hdf5 h5py netCDF4 scipy matplotlib astropy
+    else
+        conda install --yes python=$TRAVIS_PYTHON_VERSION scipy matplotlib astropy
+    fi
 fi
