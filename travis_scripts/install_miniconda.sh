4ffae0916ccace624a70be908484c1e550f82b47
mbachett <matteo.bachetti@irap.omp.eu>
2015-07-14 18:24:18 +0200

    Add numpy to conda install

--- travis_scripts/install_miniconda.sh
+++ travis_scripts/install_miniconda.sh
@@ -9,8 +9,8 @@ if test ! -e /home/travis/miniconda; then
     export PATH=/home/travis/miniconda/bin:$PATH
     conda update --yes conda
     if test WITH_NETCDF = "yes"; then
-        conda install --yes python=$TRAVIS_PYTHON_VERSION libnetcdf hdf5 h5py netCDF4 scipy matplotlib astropy
+        conda install --yes python=$TRAVIS_PYTHON_VERSION libnetcdf hdf5 h5py netCDF4 scipy matplotlib astropy numpy
     else
-        conda install --yes python=$TRAVIS_PYTHON_VERSION scipy matplotlib astropy
+        conda install --yes python=$TRAVIS_PYTHON_VERSION scipy matplotlib astropy numpy
     fi
 fi
