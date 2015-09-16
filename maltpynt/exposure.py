cb2c06e908e4276cb15fce1f61347a904461ef59
mbachett <matteo.bachetti@irap.omp.eu>
2015-09-16 17:58:44 +0200

    Correct corner case; correct exposition for bin time /= 1

--- maltpynt/exposure.py
+++ maltpynt/exposure.py
@@ -84,7 +84,7 @@ def get_livetime_per_bin(times, events, priors, dt=None, gti=None):
     pr_fl[before_start] = ev_fl[before_start] - livetime_starts[before_start]
 
     after_end = \
-        (livetime_starts < tbin_starts[-1]) & (ev_fl > tbin_starts[-1])
+        (livetime_starts < tbins[-1]) & (ev_fl > tbins[-1])
     ev_fl[after_end] = tbins[-1] - 1e-9
     pr_fl[after_end] = ev_fl[after_end] - livetime_starts[after_end]
 
@@ -235,6 +235,7 @@ def get_exposure_from_uf(time, uf_file, dt=None, gti=None):
     # filt = (grade < 32) & (pis >= 0) & (x is not None) & (y is not None)
 
     expo = get_livetime_per_bin(time, events, priors, dt, gti=gti)
+
     return expo
 
 
@@ -286,7 +287,7 @@ def correct_lightcurve(lc_file, uf_file, outname=None):
 
     outdata = contents.copy()
 
-    outdata["lc"] = np.array(lc / expo, dtype=np.float64)
+    outdata["lc"] = np.array(lc / (expo / dt), dtype=np.float64)
     outdata["expo"] = expo
 
     save_lcurve(outdata, outname)
