e48c3e8cc31ecb41e17afd9fe3d8e730d1633598
Matteo Bachetti <matteo@matteobachetti.it>
2018-01-28 15:54:29 +0100

    Fix Unicode error

--- hendrics/phasetag.py
+++ hendrics/phasetag.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from __future__ import (division, print_function, unicode_literals,
-                        absolute_import)
+from __future__ import (division, print_function, absolute_import)
 
 import argparse
 
