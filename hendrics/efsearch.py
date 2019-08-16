90cc525a30ea1780ec7178483e1e0d25828868bb
Matteo Bachetti <matteo@matteobachetti.it>
2019-08-16 17:59:55 +0200

    Experiment with speed

--- hendrics/efsearch.py
+++ hendrics/efsearch.py
@@ -18,6 +18,15 @@ import os
 import logging
 import argparse
 from functools import wraps
+
+try:
+    from fast_histogram import histogram2d
+    HAS_FAST_HIST = True
+except ImportError:
+    from numpy import histogram2d_np
+    def histogram2d(*args, **kwargs):
+        return histogram2d_np(*args, **kwargs)[0]
+
 try:
     from numba import njit, prange
 except:
@@ -169,7 +178,7 @@ def calculate_shifts(nprof: int, nbin : int, nshift : int, order : int =1) -> np
     return nshift * shifts
 
 
-@njit(parallel=True)
+@njit()
 def shift_and_select(repeated_profiles, lshift, qshift, newprof):
     nprof = len(repeated_profiles)
     nbin = len(newprof[0])
@@ -181,7 +190,19 @@ def shift_and_select(repeated_profiles, lshift, qshift, newprof):
     return newprof
 
 
-@njit()
+@njit(parallel=True)
+def shift_and_select_parallel(repeated_profiles, lshift, qshift, newprof):
+    nprof = len(repeated_profiles)
+    nbin = len(newprof[0])
+    lshifts = calculate_shifts(nprof, nbin, lshift, 1)
+    qshifts = calculate_shifts(nprof, nbin, qshift, 2)
+    for k in prange(nprof):
+        total_shift = int(np.rint(lshifts[k] + qshifts[k])) % nbin
+        newprof[k, :] = repeated_profiles[k, nbin - total_shift: 2 * nbin - total_shift]
+    return newprof
+
+
+@njit(fastmath=True)
 def z_n_fast(phase, norm, n=2):
     '''Z^2_n statistics, a` la Buccheri+03, A&A, 128, 245, eq. 2.
 
@@ -215,14 +236,71 @@ def z_n_fast(phase, norm, n=2):
     total_norm = np.sum(norm)
 
     result = 0
+    # Instead of calculating k phi each time
+    kph = np.zeros_like(phase)
+
     for k in range(1, n + 1):
-        kph = k * phase
+        kph += phase
         result += np.sum(np.cos(kph) * norm) ** 2 + np.sum(np.sin(kph) * norm) ** 2
 
     return 2 / total_norm * result
 
 
+@njit(parallel=True)
+def _fast_step(profiles, L, Q, linbinshifts, quabinshifts, nbin, n=2):
+    twopiphases = 2 * np.pi * np.arange(0, 1, 1/nbin)
+    stats = np.zeros_like(L)
+    profiles = profiles.T
+    newprof = np.zeros_like(profiles)
+    repeated_profiles = np.hstack((profiles, profiles, profiles))
+
+    for i in range(len(linbinshifts)):
+        for j in range(len(quabinshifts)):
+            newprof = shift_and_select(repeated_profiles, L[i, j], Q[i, j],
+                                       newprof)
+            splat_prof = np.sum(newprof, axis=0)
+            local_stat = z_n_fast(twopiphases, norm=splat_prof, n=n)
+            # local_stat = stat(splat_prof)
+            stats[i, j] = local_stat
+
+    return stats
+
+
 def search_with_qffa_step(times, mean_f, mean_fdot=0, nbin=16, nprof=64,
+                          npfact=2, oversample=8, n=1, search_fdot=True):
+    """Single step of quasi-fast folding algorithm."""
+    ts = times - np.mean(times)
+    phases = ts * mean_f + 0.5 * ts**2 * mean_fdot
+    phases = phases - np.floor(phases)
+    #phbins = np.linspace(0, 1, nbin + 1)
+    #tbins = np.linspace(times[0], times[-1], nprof + 1)
+    profiles = histogram2d(phases, times, range=[[0, 1], [times[0], times[-1]]],
+                            bins=(nbin, nprof))
+    print(profiles)
+    t0, t1 = times.min(), times.max()
+
+    # dn = max(1, int(nbin / oversample))
+    linbinshifts = np.linspace(-nbin * npfact, nbin * npfact, oversample * npfact)
+    if search_fdot:
+        quabinshifts = np.linspace(-nbin * npfact, nbin * npfact, oversample * npfact)
+    else:
+        quabinshifts = [0]
+
+    dphi = 1 / nbin
+    delta_t = (t1 - t0) / 2
+    df = dphi / delta_t
+    dfdot = 2 * dphi / delta_t ** 2
+
+    bin_to_frequency = df
+    bin_to_fdot = dfdot
+    L, Q = np.meshgrid(linbinshifts, quabinshifts, indexing='ij')
+
+    stats = _fast_step(profiles, L, Q, linbinshifts, quabinshifts, nbin, n=n)
+
+    return L * bin_to_frequency + mean_f, Q * bin_to_fdot + mean_fdot, stats
+
+
+def search_with_qffa_step_old(times, mean_f, mean_fdot=0, nbin=16, nprof=64,
                           npfact=2, oversample=8, n=1, search_fdot=True):
     """Single step of quasi-fast folding algorithm."""
     ts = times - np.mean(times)
@@ -252,8 +330,6 @@ def search_with_qffa_step(times, mean_f, mean_fdot=0, nbin=16, nprof=64,
     bin_to_frequency = df
     bin_to_fdot = dfdot
 
-    factor = bin_to_frequency
-
     stats = np.zeros_like(L)
     profiles = profiles.T
     newprof = np.zeros_like(profiles)
@@ -262,7 +338,7 @@ def search_with_qffa_step(times, mean_f, mean_fdot=0, nbin=16, nprof=64,
     twopiphases = 2 * np.pi * np.arange(0, 1, 1/nbin)
     for i, l in enumerate(linbinshifts):
         for j, q in enumerate(quabinshifts):
-            newprof = shift_and_select(repeated_profiles, L[i, j], Q[i, j],
+            newprof = shift_and_select_parallel(repeated_profiles, L[i, j], Q[i, j],
                                        newprof)
             splat_prof = np.sum(newprof, axis=0)
             local_stat = z_n_fast(twopiphases, norm=splat_prof, n=n)
