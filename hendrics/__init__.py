86492a83631e0effca3af0ee98c0241ea138f0eb
Matteo Bachetti <matteo@matteobachetti.it>
2019-12-14 15:03:45 +0100

    Fix readthedocs (#74)
    Make it build on readthedocs

--- hendrics/__init__.py
+++ hendrics/__init__.py
@@ -14,36 +14,12 @@ if not _ASTROPY_SETUP_:
     # Workaround: import netCDF4 before everything else. This loads the HDF5
     # library that netCDF4 uses and not something else.
 
-    import warnings
     try:
         import netCDF4 as nc
 
         HEN_FILE_EXTENSION = '.nc'
         HAS_NETCDF = True
     except ImportError:
-        msg = "Warning! NetCDF is not available. Using pickle format."
-        warnings.warn(msg)
         HEN_FILE_EXTENSION = '.p'
         HAS_NETCDF = False
         pass
-
-    from . import base
-    from . import binary
-    from . import calibrate
-    from . import colors
-    from . import create_gti
-    from . import efsearch
-    from . import exposure
-    from . import exvar
-    from . import fake
-    from . import fspec
-    from . import io
-    from . import lcurve
-    from . import modeling
-    from . import plot
-    from . import read_events
-    from . import rebin
-    from . import save_as_xspec
-    from . import sum_fspec
-    from . import timelags
-    from . import varenergy
