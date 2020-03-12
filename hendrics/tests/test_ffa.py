e5cea598306f5e24e6a714a2acf17b01c4083a38
Matteo Bachetti <matteo@matteobachetti.it>
2020-03-12 10:24:21 +0100

    Test FFA without numba, test more thoroughly

--- hendrics/tests/test_ffa.py
+++ hendrics/tests/test_ffa.py
@@ -7,13 +7,13 @@ from ..ffa import ffa_search
 from ..efsearch import fit
 import pytest
 
-@pytest.mark.skipif('not HAS_NUMBA')
+# @pytest.mark.skipif('not HAS_NUMBA')
 def test_ffa():
     period = 0.01
-    pmin = 0.009
-    pmax = 0.011
+    pmin = 0.0095
+    pmax = 0.0105
     dt = 10**int(np.log10(period)) / 256
-    length = 10
+    length = 1
     times = np.arange(0, length, dt)
 
     flux = 10 + np.cos(2 * np.pi * times / period)
@@ -30,7 +30,30 @@ def test_ffa():
     assert np.isclose(1/model.mean, period, atol=1e-6)
 
 
-@pytest.mark.skipif('not HAS_NUMBA')
+# @pytest.mark.skipif('not HAS_NUMBA')
+def test_ffa_large_intv():
+    period = 0.01
+    pmin = 0.002789345
+    pmax = 0.0105
+    dt = 10**int(np.log10(0.002789345)) / 20
+    length = 5
+    times = np.arange(0, length, dt)
+
+    flux = 10 + np.cos(2 * np.pi * times / period)
+
+    lc_cont = Lightcurve(times, flux, err_dist='gauss')
+
+    ev = EventList()
+    ev.simulate_times(lc_cont)
+    lc = Lightcurve.make_lightcurve(ev.time, dt=dt, tstart=0, tseg=length)
+
+    per, st = ffa_search(lc.counts, dt, pmin, pmax)
+    #  fit_sinc wants frequencies, not periods
+    model = fit(1/per[::-1], st[::-1], 1/period, obs_length=10)
+    assert np.isclose(1/model.mean, period, atol=1e-6)
+
+
+# @pytest.mark.skipif('not HAS_NUMBA')
 
 def test_ffa_vs_folding_search():
     import time
