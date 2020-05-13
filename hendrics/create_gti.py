390fabbed8b140c021ad9864f1ba7c3bd1efdf59
Matteo Bachetti <matteo@matteobachetti.it>
2020-05-13 17:57:37 +0200

    Fix GTI filtering in new event files

--- hendrics/create_gti.py
+++ hendrics/create_gti.py
@@ -107,7 +107,10 @@ def apply_gti(fname, gti, outname=None,
         data['counts'] = data['counts'][good]
         data['counts_err'] = data['counts_err'][good]
     elif ftype == 'events':
-        data['PI'] = data['PI'][good]
+        for key in ['pi', 'energy']:
+            if key in data:
+                data[key] = data[key][good]
+
         if data['instr'] == 'PCA':  # pragma: no cover
             data['PCU'] = data['PCU'][good]
 
