3fbcb05a4fd3dfb9f23af223638a55afe127881d
Matteo Bachetti <matteo@matteobachetti.it>
2021-03-22 14:32:39 +0100

    Proper monkeypatching

--- hendrics/__init__.py
+++ hendrics/__init__.py
@@ -25,6 +25,7 @@ if not _ASTROPY_SETUP_:
         pass
 
     import stingray.utils
+    from stingray.events import EventList
 
     # Patch stingray.utils
     def _root_squared_mean(array):
@@ -34,3 +35,38 @@ if not _ASTROPY_SETUP_:
         return np.sqrt(np.sum(array ** 2)) / len(array)
 
     stingray.utils._root_squared_mean = _root_squared_mean
+
+    class MonkeyPatchedEventList(EventList):
+        def apply_mask(self, mask, inplace=False):  # pragma: no cover
+            """For compatibility with old stingray version.
+
+            Examples
+            --------
+            >>> evt = MonkeyPatchedEventList(time=[0, 1, 2])
+            >>> newev0 = evt.apply_mask([True, True, False], inplace=False);
+            >>> newev1 = evt.apply_mask([True, True, False], inplace=True);
+            >>> np.allclose(newev0.time, [0, 1])
+            True
+            >>> np.allclose(newev1.time, [0, 1])
+            True
+            >>> evt is newev1
+            True
+            """
+            import copy
+            if inplace:
+                new_ev = self
+            else:
+                new_ev = copy.deepcopy(self)
+            for attr in "time", "energy", "pi", "cal_pi":
+                if hasattr(new_ev, attr) and getattr(new_ev, attr) is not None:
+                    setattr(new_ev, attr, getattr(new_ev, attr)[mask])
+            return new_ev
+
+    try:
+        e = EventList(time=[1, 2, 3])
+        e.energy = None
+        e.apply_mask([True, True, False])
+        print(e)
+    except TypeError:
+        print("Monkey patching Eventlist")
+        stingray.events.EventList = MonkeyPatchedEventList
