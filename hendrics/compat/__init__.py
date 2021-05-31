7c6ae83a0349a185a110e7ab37938ef1dce8e149
Matteo Bachetti <matteo@matteobachetti.it>
2021-05-31 18:29:37 +0200

    Fix Monkeypatcheventlist

--- hendrics/compat/__init__.py
+++ hendrics/compat/__init__.py
@@ -1,12 +1,4 @@
 from .compatibility import (
-    _MonkeyPatchedEventList,
-    filter_for_deadtime,
-    get_deadtime_mask,
-    read_mission_info,
-    _case_insensitive_search_in_list,
-    get_key_from_mission_info,
-)
-from .compatibility import (
     prange,
     array_take,
     HAS_NUMBA,
