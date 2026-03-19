4659435fe7136de03c0d5072d69cfd6990967b7f
Matteo Bachetti <matteo@matteobachetti.it>
2026-03-19 11:03:42 +0100

    Add exclude pattern [docs only]

--- docs/conf.py
+++ docs/conf.py
@@ -70,6 +70,7 @@ with (Path(__file__).parents[1] / "pyproject.toml").open("rb") as f:
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns.append("_templates")
+exclude_patterns.append("changes")
 
 # This is added to the end of RST files - a good place to put substitutions to
 # be used globally.
